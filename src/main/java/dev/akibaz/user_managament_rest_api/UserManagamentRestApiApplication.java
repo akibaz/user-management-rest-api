44f8fa9d7056e56f7891914903df9e864471095f
akibaz <bavakumovic@gmail.com>
2026-01-07 18:59:29 +0100

    Initial bootstrap

---
+++ src/main/java/dev/akibaz/user_managament_rest_api/UserManagamentRestApiApplication.java
@@ -0,0 +1,13 @@
+package dev.akibaz.user_managament_rest_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManagamentRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserManagamentRestApiApplication.class, args);
+	}
+
+}
